097d7ac12c0877733c3d7bc8a26c402cdd3794e6
Guy SHARONY <gsharony@made-f0Dr2s3.clusters.42paris.fr>
2023-11-17 12:54:27 +0100

    Flag

--- robots/Ressources/robots.py
+++ robots/Ressources/robots.py
@@ -2,8 +2,8 @@ import requests
 import urllib.parse
 
 
-def try_robots(path: str):
-    url = f'http://192.168.56.101/.hidden/{path}'
+def try_robots(path: str = ""):
+    url = f'http://192.168.56.101/.hidden{path}'
 
     response = requests.get(url)
 
@@ -11,21 +11,31 @@ def try_robots(path: str):
 
 
 def filter_index(path: str = "/"):
-    text = try_robots()
+    text = try_robots(path)
 
     lines = text.split('\n')
 
     links = [x for x in lines if x.startswith('<a href=')]
+    links.reverse()
+
     for link in links:
         if type(link) == str:
             start_index = link.find('"') + 1
             end_index = link.find('"', start_index + 1)
             if start_index != -1 and end_index != -1:
-                print(path + link[start_index:end_index])
+                file = link[start_index:end_index]
+                new_path = path + file
+                if file != "README":
+                    filter_index(new_path)
+                else:
+                    text = try_robots(new_path)
+                    print(f"[{new_path}]: ", text)
+                    if len(text) > 40:
+                        exit()
 
 
 def main():
-    print(filter_index(response))
+    print(filter_index("/"))
 
 
 if __name__ == "__main__":
